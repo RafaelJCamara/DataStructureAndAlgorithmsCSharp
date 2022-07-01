be22fe90f613af64e62ef8f5a90ec16e062df56b
Rafael Camara <rafaelcamarac@gmail.com>
2022-07-01 22:39:25 +0100

    Add HashTable base class and notes.

---
+++ DSA/DSA/HashTables/CustomHashTable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace DSA.HashTables
+{
+    public class CustomHashTable
+    {
+
+        private object[] _dictionary;
+
+        public CustomHashTable()
+        {
+            this._dictionary = new object[100];
+        }
+
+        public void Add(int key, object obejct)
+        {
+
+        }
+
+        public object Get(int key)
+        {
+
+        }
+
+        public bool ContainsKey(int key)
+        {
+
+        }
+
+        public void Remove(int key)
+        {
+
+        }
+
+    }
+}
+
---
+++ DSA/DSA/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DSA
+{
+    public class Program
+    {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello world.");
+        }
+    }
+}
+
