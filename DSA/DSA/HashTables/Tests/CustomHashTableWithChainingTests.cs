053deb426069927e7355d04ffdf868ec61449053
Rafael Camara <rafaelcamarac@gmail.com>
2022-07-02 16:37:21 +0100

    Add simple unit testing.

---
+++ DSA/DSA/HashTables/Tests/CustomHashTableWithChainingTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+
+namespace DSA.HashTables.Tests
+{
+    public class CustomHashTableWithChainingTests
+    {
+
+        [Theory]
+        [InlineData(1, "One")]
+        [InlineData(2, "Two")]
+        [InlineData(3, "Three")]
+        public void Get_RetrieveStoredValueForAnExistingKey(int key, string value)
+        {
+            //Arrange
+            var hashTable = new CustomHashTableWithChaining();
+
+            //Act
+            hashTable.Add(key,value);
+            var retrievedValue = hashTable.Get(key);
+
+            //Assert
+            Assert.Equal(value, retrievedValue);
+        }
+    }
+}
+
