5840721c5ac80e530613f48fd170414ea1a53678
RafaelJCamara <rafaelcamarac@gmai.com>
2022-07-02 18:21:06 +0100

    Add tests to method ContainsKey and Get, from Hash Table.

--- DSA/DSA/HashTables/Tests/CustomHashTableWithChainingTests.cs
+++ DSA/DSA/HashTables/Tests/CustomHashTableWithChainingTests.cs
@@ -16,12 +16,65 @@ namespace DSA.HashTables.Tests
             var hashTable = new CustomHashTableWithChaining();
 
             //Act
-            hashTable.Add(key,value);
+            hashTable.Add(key, value);
             var retrievedValue = hashTable.Get(key);
 
             //Assert
             Assert.Equal(value, retrievedValue);
         }
+
+        [Theory]
+        [InlineData(1)]
+        public void Get_ThrowsExceptionWhenLookingForNonExistingKey(int key)
+        {
+            //Arrange
+            var hashTable = new CustomHashTableWithChaining();
+            var expectedErrorMessage = "Key not found!";
+
+            //Act
+
+            //Assert
+            var exception = Assert.Throws<Exception>(() => hashTable.Get(key));
+            Assert.Equal(expectedErrorMessage, exception.Message);
+        }
+
+
+        [Theory]
+        [MemberData(nameof(TestGetValueForAGivenKey))]
+
+        public void ContainsKey_RetrievesTrueOrFalseDependingIfTheKeyExistsOrNot(
+            List<int> keys, List<string> values, int key, bool expected)
+        {
+            //Arrange
+            var hashTable = new CustomHashTableWithChaining();
+            for(int i = 0; i != keys.Count; i++)
+            {
+                hashTable.Add(keys[i], values[i]);
+            }
+
+            //Act
+            var doesKeyExists = hashTable.ContainsKey(key);
+
+            //Assert
+            Assert.Equal(expected, doesKeyExists);
+        }
+
+        public static IEnumerable<object[]> TestGetValueForAGivenKey
+        {
+            get
+            {
+                var keys = new List<int> { 1, 2, 3, 101, 102, 103 };
+                var values = new List<string> { "one", "two", "three", "one0one", "one0two", "one0three" };
+                return new List<object[]>
+                        {
+                            new object[] { keys, values, 1, true },
+                            new object[] { keys, values, 102, true },
+                            new object[] { keys, values, 154, false },
+                            new object[] { keys, values, 289, false }
+                        };
+            }
+        }
+
     }
 }
 
