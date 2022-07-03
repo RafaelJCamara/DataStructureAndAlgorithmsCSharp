61e1ed8a1654a27131f02d572f07c6114aecb1ba
RafaelJCamara <rafaelcamarac@gmai.com>
2022-07-03 19:27:32 +0100

    Add LinkedList implementation and respective tests.

--- DSA/DSA/LinkedList/CustomLinkedList.cs
+++ DSA/DSA/LinkedList/CustomLinkedList.cs
@@ -20,8 +20,8 @@ namespace DSA.LinkedList
             {
                 Value = value
             };
-            
-            if(Head == null)
+
+            if (Head == null)
             {
                 Tail = node;
             }
@@ -29,13 +29,94 @@ namespace DSA.LinkedList
             {
                 node.Next = Head;
             }
+
             Head = node;
         }
 
-        //AddLast
-        //RemoveFirst
-        //RemoveLast
-        //Contains
-        //IndexOf
+        /*
+            Time Complexity: O(1)
+        */
+        public void AddLast(int value)
+        {
+            var node = new CustomNode
+            {
+                Value = value
+            };
+
+            if(Head == null)
+            {
+                Head = node;
+            }
+            else
+            {
+                Tail.Next = node;
+            }
+
+            Tail = node;
+        }
+
+        /*
+            Time Complexity: O(1)
+        */
+        public void RemoveFirst()
+        {
+            if (Head == null) throw new NullReferenceException("No nodes to be removed!");
+            var aux = Head;
+            Head = Head.Next;
+            aux.Next = null;
+            if (Head == null) Tail = null;
+        }
+
+        /*
+            Time Complexity: O(n)
+        */
+        public void RemoveLast()
+        {
+            if (Head == null) throw new NullReferenceException("No nodes to be removed!");
+            if (Head == Tail)
+            {
+                Head = null;
+                Tail = null;
+                return;
+            }
+            CustomNode aux = Head;
+            while (aux.Next.Next != null)
+            {
+                aux = aux.Next;
+            }
+            aux.Next = null;
+            Tail = aux;
+        }
+
+        /*
+            Time Complexity: O(n)
+        */
+        public bool Contains(int value)
+        {
+            var aux = Head;
+            while (aux != null)
+            {
+                if (aux.Value == value) return true;
+                aux = aux.Next;
+            }
+            return false;
+        }
+
+        /*
+            Time Complexity: O(n)
+        */
+        public int IndexOf(int value)
+        {
+            var aux = Head;
+            int index = 0;
+            while(aux != null)
+            {
+                if(aux.Value == value) return index;
+                index += 1;
+                aux = aux.Next;
+            }
+            return -1;
+        }
+
     }
 }
---
+++ DSA/DSA/LinkedList/Tests/CustomLinkedListTests.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DSA.LinkedList.Tests
+{
+    public class CustomLinkedListTests
+    {
+        [Theory]
+        [InlineData(1)]
+        public void AddFirst_AddFirstElementWhenLinkedListIsEmpty(int value)
+        {
+            //Arrange
+            var linkedList = new CustomLinkedList();
+
+            //Act
+            linkedList.AddFirst(value);
+
+            //Assert
+            Assert.Equal(value, linkedList.Head.Value);
+            Assert.Equal(linkedList.Head.Value, linkedList.Tail.Value);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        public void AddFirst_AddFirstElementWhenLinkedListIsNotEmpty(int value)
+        {
+            //Arrange
+            var linkedList = new CustomLinkedList();
+            for(int i = 100; i < 201; i++) linkedList.AddFirst(i);
+
+            //Act
+            linkedList.AddFirst(value);
+
+            //Assert
+            Assert.Equal(value, linkedList.Head.Value);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        public void AddLast_AddLastElementWhenLinkedListIsEmpty(int value)
+        {
+            //Arrange
+            var linkedList = new CustomLinkedList();
+
+            //Act
+            linkedList.AddLast(value);
+
+            //Assert
+            Assert.Equal(value, linkedList.Tail.Value);
+            Assert.Equal(linkedList.Tail.Value, linkedList.Head.Value);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        public void AddLast_AddLastElementWhenLinkedListIsNotEmpty(int value)
+        {
+            //Arrange
+            var linkedList = new CustomLinkedList();
+            for (int i = 100; i < 201; i++) linkedList.AddLast(i);
+
+            //Act
+            linkedList.AddLast(value);
+
+            //Assert
+            Assert.Equal(value, linkedList.Tail.Value);
+        }
+
+        [Fact]
+        public void RemoveFirst_RemovesFirstElementOfNonEmptyLinkedListWithSingleElement()
+        {
+            //Arrange
+            var linkedList = new CustomLinkedList();
+            linkedList.AddFirst(1);
+
+            //Act
+            linkedList.RemoveFirst();
+
+            //Assert
+            Assert.Null(linkedList.Head);
+            Assert.Null(linkedList.Tail);
+        }
+
+        [Fact]
+        public void RemoveFirst_RemovesFirstElementOfNonEmptyLinkedListWithMultipleElements()
+        {
+            //Arrange
+            var linkedList = new CustomLinkedList();
+            for (int i = 100; i < 201; i++) linkedList.AddLast(i);
+            var oldHeadValue = linkedList.Head.Value;
+
+            //Act
+            linkedList.RemoveFirst();
+            var newHeadValue = linkedList.Head.Value;
+
+            //Assert
+            Assert.NotNull(linkedList.Head);
+            Assert.NotNull(linkedList.Tail);
+            Assert.NotEqual(oldHeadValue, newHeadValue); //we are assuming that node values are unique, although there is no proper validation
+        }
+
+        [Fact]
+        public void RemoveFirst_ThrowsExceptionWhenRemovingFirstElementOfEmptyLinkedList()
+        {
+            //Arrange
+            var linkedList = new CustomLinkedList();
+            var expectedExceptionMessage = "No nodes to be removed!";
+
+            //Act
+
+            //Assert
+            Assert.Null(linkedList.Head);
+            Assert.Null(linkedList.Tail);
+            var exception = Assert.Throws<NullReferenceException>(() => linkedList.RemoveFirst());
+            Assert.Equal(expectedExceptionMessage, exception.Message);
+        }
+
+        [Fact]
+        public void RemoveLast_RemovesLastElementOfNonEmptyLinkedListWithSingleElement()
+        {
+            //Arrange
+            var linkedList = new CustomLinkedList();
+            linkedList.AddLast(1);
+
+            //Act
+            linkedList.RemoveLast();
+
+            //Assert
+            Assert.Null(linkedList.Head);
+            Assert.Null(linkedList.Tail);
+        }
+
+        [Fact]
+        public void RemoveLast_RemovesLastElementOfNonEmptyLinkedListWithMultipleElements()
+        {
+            //Arrange
+            var linkedList = new CustomLinkedList();
+            for (int i = 100; i < 201; i++) linkedList.AddLast(i);
+            var oldTailValue = linkedList.Tail.Value;
+
+            //Act
+            linkedList.RemoveLast();
+            var newTailValue = linkedList.Tail.Value;
+
+            //Assert
+            Assert.NotNull(linkedList.Head);
+            Assert.NotNull(linkedList.Tail);
+            Assert.NotEqual(oldTailValue, newTailValue); //we are assuming that node values are unique, although there is no proper validation
+        }
+
+        [Fact]
+        public void RemoveLast_ThrowsExceptionWhenRemovingFirstElementOfEmptyLinkedList()
+        {
+            //Arrange
+            var linkedList = new CustomLinkedList();
+            var expectedExceptionMessage = "No nodes to be removed!";
+
+            //Act
+
+            //Assert
+            Assert.Null(linkedList.Head);
+            Assert.Null(linkedList.Tail);
+            var exception = Assert.Throws<NullReferenceException>(() => linkedList.RemoveLast());
+            Assert.Equal(expectedExceptionMessage, exception.Message);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestContainsValue))]
+        public void Contains(List<int> values, int valueToSearch, bool expectedContains)
+        {
+            //Arrange
+            var linkedList = new CustomLinkedList();
+            foreach (int value in values) linkedList.AddLast(value);
+
+            //Act
+            var containsValue = linkedList.Contains(valueToSearch);
+
+            //Assert
+            Assert.Equal(expectedContains, containsValue);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestIndexOfValue))]
+        public void IndexOf(List<int> values, int valueToSearch, int expectedIndex)
+        {
+            //Arrange
+            var linkedList = new CustomLinkedList();
+            foreach (int value in values) linkedList.AddLast(value);
+
+            //Act
+            var indexOf = linkedList.IndexOf(valueToSearch);
+
+            //Assert
+            Assert.Equal(expectedIndex, indexOf);
+        }
+
+
+        public static IEnumerable<object[]> TestContainsValue
+        {
+            get
+            {
+                var values = new List<int> { 1, 2, 3, 101, 102, 103 };
+                return new List<object[]>
+                        {
+                            new object[] { values, 1, true },
+                            new object[] { values, 102, true },
+                            new object[] { values, 154, false },
+                            new object[] { values, 289, false }
+                        };
+            }
+        }
+
+        public static IEnumerable<object[]> TestIndexOfValue
+        {
+            get
+            {
+                var values = new List<int> { 1, 2, 3, 101, 102, 103 };
+                return new List<object[]>
+                        {
+                            new object[] { values, 1, 0 },
+                            new object[] { values, 102, 4 },
+                            new object[] { values, 103, 5 },
+                            new object[] { values, 154, -1 },
+                            new object[] { values, 289, -1 }
+                        };
+            }
+        }
+
+    }
+}
