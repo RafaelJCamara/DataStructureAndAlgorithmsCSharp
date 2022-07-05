a925a01a9e08cacaf187a5cc579f243d8db101b2
Rafael Camara <rafaelcamarac@gmail.com>
2022-07-05 07:19:11 +0100

    Trying PriorityQueues.

--- DSA/DSA/Program.cs
+++ DSA/DSA/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using DSA.HashTables;
+using DSA.Queues.PriorityQueues;
 
 namespace DSA
 {
     public class Program
     {
         public static void Main(string[] args) {
+            /*
             var hashTable = new CustomHashTableWithChaining();
             hashTable.Add(0, "zero");
             hashTable.Add(1, "one");
@@ -19,6 +21,17 @@ namespace DSA
             hashTable.Remove(0);
             Console.WriteLine(hashTable.ContainsKey(0));
             hashTable.Remove(56);
+            */
+            var priorityQueue = new PriorityQueue<string, int>(new ElementComparer());
+            priorityQueue.Enqueue("A", 4);
+            priorityQueue.Enqueue("B", 1);
+            priorityQueue.Enqueue("C", 2);
+            priorityQueue.Enqueue("D", 5);
+            priorityQueue.Enqueue("E", 3);
+            while (priorityQueue.TryDequeue(out string item, out int priority))
+            {
+                Console.WriteLine($"Popped Item : {item}. Priority Was : {priority}");
+            }
         }
     }
 }
---
+++ DSA/DSA/Queues/PriorityQueues/ElementComparer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace DSA.Queues.PriorityQueues
+{
+    public class ElementComparer : IComparer<Int32>
+    {
+        public int Compare(int x, int y)
+        {
+            if (x == y)
+            {
+                return 0;
+            }
+            return x - y;
+        }
+    }
+}
+
