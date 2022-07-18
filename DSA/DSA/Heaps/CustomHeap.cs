f4a51f1a597996546d6774bb4f2672a9b7e69deb
Rafael Camara <rafaelcamarac@gmail.com>
2022-07-18 08:25:24 +0100

    Add more heap notes and basic class placehold.

---
+++ DSA/DSA/Heaps/CustomHeap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace DSA.Heaps
+{
+    public class CustomHeap
+    {
+        public int[] Heap { get; set; }
+        public int Size { get; private set; }
+
+        public CustomHeap(int size)
+        {
+            Size = size;
+            Heap = new int[Size];
+        }
+
+        //insert(int)
+
+        //remove()
+
+    }
+}
+
