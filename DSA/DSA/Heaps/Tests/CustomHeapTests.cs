2739894fe510031b07ca807ba3833b9533d3c87a
Rafael Camara <rafaelcamarac@gmail.com>
2022-07-21 17:54:47 +0100

    Add Heap Insert and Remove tests.

--- DSA/DSA/Heaps/Tests/CustomHeapTests.cs
+++ DSA/DSA/Heaps/Tests/CustomHeapTests.cs
@@ -38,15 +38,33 @@ namespace DSA.Heaps.Tests
             Assert.Equal(expectedHeapAfterInsert, customHeap.Heap);
         }
 
-
+        [Fact]
         public void Remove_ThrowsExceptionWhenHeapIsEmpty()
         {
+            //Arrange
+            var customHeap = new CustomHeap(1);
+            var expectedExceptionMessage = "Heap is empty!";
 
+            //Act
+
+            //Assert
+            var exception = Assert.Throws<InvalidOperationException>(() => customHeap.Remove());
+            Assert.Equal(expectedExceptionMessage, exception.Message);
         }
 
-        public void Remove_RemovesElementFromHeapAndLeavesHeapInAValidState()
+        [Theory]
+        [MemberData(nameof(TestRemoveFromHeapData))]
+        public void Remove_RemovesElementFromHeapAndLeavesHeapInAValidState(int size, int[] initValues, int[] expectedFinalHeap)
         {
+            //Arrange
+            var customHeap = new CustomHeap(size);
+            foreach (var value in initValues) customHeap.Insert(value);
 
+            //Act
+            customHeap.Remove();
+
+            //Assert
+            Assert.Equal(expectedFinalHeap, customHeap.Heap);
         }
 
         public static IEnumerable<object[]> TestInsertValueInHeapData
@@ -61,6 +79,24 @@ namespace DSA.Heaps.Tests
                         {
                             new object[] { values1, values1},
                             new object[] { values2, expectedValues2 }
+                        };
+            }
+        }
+
+        public static IEnumerable<object[]> TestRemoveFromHeapData
+        {
+            get
+            {
+                var valuesInit1 = new int[] { 40, 30, 20, 10 };
+                var valuesFinal1 = new int[] { 30, 10, 20, 0 };
+
+                var valuesInit2 = new int[] { 40, 30, 20 };
+                var valuesFinal2 = new int[] { 30, 20, 0 };
+
+                return new List<object[]>
+                        {
+                            new object[] { 4, valuesInit1, valuesFinal1},
+                            new object[] { 3, valuesInit2, valuesFinal2 }
                         };
             }
         }
