c3c1e4528d58a3f2f7d7aaa791e3dc97fcabdc91
Rafael Camara <rafaelcamarac@gmail.com>
2022-07-29 08:05:46 +0100

    Add basic Trie Insert method using arrays.

---
+++ DSA/DSA/Tries/Trie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace DSA.Tries
+{
+    public class Trie
+    {
+        public TrieNode Root { get; set; }
+
+        public Trie()
+        {
+            Root = new TrieNode(' ');
+        }
+
+        public void Insert(string word)
+        {
+            var current = Root;
+            foreach(var character in word){
+                int index = character - 'a';
+                if (current.Children[index] == null)
+                    current.Children[index] = new TrieNode(character);
+                current = current.Children[index];
+            }
+            current.IsEndOfWord = true;
+        }
+
+    }
+}
+
---
+++ DSA/DSA/Tries/TrieNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace DSA.Tries
+{
+    public class TrieNode
+    {
+        private int _englishAlphabetSize = 26;
+        public char Value { get; private set; }
+        public TrieNode[] Children { get; set; }
+        public bool IsEndOfWord { get; set; }
+
+        public TrieNode(char value)
+        {
+            Children = new TrieNode[_englishAlphabetSize];
+            Value = value;
+        }
+
+    }
+}
+
